0b23ec0a2ca8918f4d95ce6ed9f13a9a12eb65d4
Andres <barathum196@gmail.com>
2018-02-26 20:23:21 -0500

    Funcionalidad sumar terminada sin validaciones

---
+++ app/src/main/java/com/holamundo/operaciones/Metodos.java
@@ -0,0 +1,13 @@
+package com.holamundo.operaciones;
+
+/**
+ * Created by Andres "Bara" Martinez on 26/02/2018.
+ */
+
+public class Metodos {
+
+    public static double sumar(double n1 , double n2){
+        return n1+n2;
+    }
+
+}
